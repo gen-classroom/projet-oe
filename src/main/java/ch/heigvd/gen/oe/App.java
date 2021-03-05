11409edc6f91179a713f908d484340a151fe166b
K-do <miguel.dovalelopes@heig-vd.ch>
2021-03-05 12:03:03 +0100

    Projet Maven crée, ajout de dépendance <lombock>

---
+++ src/main/java/ch/heigvd/gen/oe/App.java
@@ -0,0 +1,15 @@
+package ch.heigvd.gen.oe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
