9c7cd1aa242c4496f50155ff7a45ad09f41c6a04
K-do <miguel.dovalelopes@heig-vd.ch>
2021-03-20 14:48:18 +0100

    added comments

--- src/main/java/ch/heigvd/gen/oe/utils/Json.java
+++ src/main/java/ch/heigvd/gen/oe/utils/Json.java
@@ -10,24 +10,22 @@ public class Json {
     /**
      * Read a json file and return an instance of the class defined
      * @param filename name of json file
-     * @param type type of class where to retrieve json data
+     * @param type type of class to return
      * @param <T> class where to retrieve data
      * @return an instance of the class defined or null if an I/O error occurred
      */
     public <T> T read(String filename, Class<T> type){
 
-        // try-with-ressources
+        /* Open, read and parse json file */
         try (BufferedReader in = new BufferedReader(new InputStreamReader(
                 new FileInputStream(filename), StandardCharsets.UTF_8))) {
 
             Gson gson = new Gson();
             return gson.fromJson(in, type);
 
-        } catch (IOException e) {
-
+        } catch (IOException e) { /* File could not be opened or read */
             e.printStackTrace();
             return null;
-
         }
 
     }
