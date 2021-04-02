cdc05f94592d89c2e48fbda0a51e4566cd4898ad
K-do <miguel.dovalelopes@heig-vd.ch>
2021-04-02 21:55:30 +0200

    fixed strings written in java 15 to java 11

--- src/test/java/ch/heigvd/gen/oe/utils/MarkdownTest.java
+++ src/test/java/ch/heigvd/gen/oe/utils/MarkdownTest.java
@@ -24,107 +24,91 @@ public class MarkdownTest {
     @Test
     public void toHtmlShouldReturnTheMarkdownStringinHtmlFormat() {
 
-        String inputMarkdown = """
-                # Sample Markdown
-                                
-                This is some basic, sample markdown.
-                                
-                ## Second Heading
-                                
-                 * Unordered lists, and:
-                                
-                1.  One
-                2.  Two
-                3.  Three
-                                
-                 * More
-                                
-                > Blockquote
-                                
-                And **bold**, *italics*, and even *italics and later **bold***. Even ~~strikethrough~~. [A link](https://markdowntohtml.com) to somewhere.
-                                
-                And code highlighting:
-                                
-                ```js
-                var foo = 'bar';
-                                
-                function baz(s) {
-                   return foo + ':' + s;
-                }
-                ```
-                                
-                Or inline code like `var foo = 'bar';`.
-                                
-                Or an image of bears
-                                
-                ![bears](http://placebear.com/200/200)
-                                
-                The end ...
-                """;
+        String inputMarkdown = "# Sample Markdown\n"
+                + "This is some basic, sample markdown.\n"
+                + "## Second Heading\n"
+                + "* Unordered lists, and:\n"
+                + "1.  One\n"
+                + "2.  Two\n"
+                + "3.  Three\n"
+                + "* More\n"
+                + "> Blockquote\n"
+                + "\n"
+                +"And **bold**, *italics*, and even *italics and later **bold***. Even ~~strikethrough~~. [A link](https://markdowntohtml.com) to somewhere.\n"
+                + "\n"
+                + "And code highlighting:\n"
+                + "```js\n"
+                + "var foo = 'bar';\n"
+                + "function baz(s) {\n"
+                + "return foo + ':' + s;\n"
+                + "}\n"
+                + "```\n"
+                + "Or inline code like `var foo = 'bar';`.\n"
+                + "\n"
+                + "Or an image of bears\n"
+                + "\n"
+                + "![bears](http://placebear.com/200/200)\n"
+                + "\n"
+                + "The end ...";
 
         String outputHtml;
 
-        final String correctHtml = """
-                <h1>Sample Markdown</h1>
-                <p>This is some basic, sample markdown.</p>
-                <h2>Second Heading</h2>
-                <ul>
-                <li>Unordered lists, and:</li>
-                </ul>
-                <ol>
-                <li>One</li>
-                <li>Two</li>
-                <li>Three</li>
-                </ol>
-                <ul>
-                <li>More</li>
-                </ul>
-                <blockquote>
-                <p>Blockquote</p>
-                </blockquote>
-                <p>And <strong>bold</strong>, <em>italics</em>, and even <em>italics and later <strong>bold</strong></em>. Even <del>strikethrough</del>. <a href="https://markdowntohtml.com">A link</a> to somewhere.</p>
-                <p>And code highlighting:</p>
-                <pre><code class="language-js">var foo = 'bar';
-                                
-                function baz(s) {
-                   return foo + ':' + s;
-                }
-                </code></pre>
-                <p>Or inline code like <code>var foo = 'bar';</code>.</p>
-                <p>Or an image of bears</p>
-                <p><img src="http://placebear.com/200/200" alt="bears" /></p>
-                <p>The end ...</p>
-                """;
+        final String correctHtml = "<h1>Sample Markdown</h1>\n"
+                + "<p>This is some basic, sample markdown.</p>\n"
+                + "<h2>Second Heading</h2>\n"
+                + "<ul>\n"
+                + "<li>Unordered lists, and:</li>\n"
+                + "</ul>\n"
+                + "<ol>\n"
+                + "<li>One</li>\n"
+                + "<li>Two</li>\n"
+                + "<li>Three</li>\n"
+                + "</ol>\n"
+                + "<ul>\n"
+                + "<li>More</li>\n"
+                + "</ul>\n"
+                + "<blockquote>\n"
+                + "<p>Blockquote</p>\n"
+                + "</blockquote>\n"
+                + "<p>And <strong>bold</strong>, <em>italics</em>, and even <em>italics and later <strong>bold</strong></em>. Even <del>strikethrough</del>. <a href=\"https://markdowntohtml.com\">A link</a> to somewhere.</p>\n"
+                + "<p>And code highlighting:</p>\n"
+                + "<pre><code class=\"language-js\">var foo = 'bar';\n"
+
+                + "function baz(s) {\n"
+                +   "return foo + ':' + s;\n"
+                + "}\n"
+                + "</code></pre>\n"
+                + "<p>Or inline code like <code>var foo = 'bar';</code>.</p>\n"
+                + "<p>Or an image of bears</p>\n"
+                + "<p><img src=\"http://placebear.com/200/200\" alt=\"bears\" /></p>\n"
+                + "<p>The end ...</p>\n";
+
 
         outputHtml = markdown.toHtml(inputMarkdown);
 
         assertEquals(correctHtml,outputHtml);
     }
 
+
     @Test
     public void getMetadataShouldSeparateMetadataAndMarkdownAndPlaceThemInTwoStrings() {
-        String markDownWithMetadata =
-                """
-                titre: Mon premier article
-                auteur: Bertil Chapuis
-                date: 2021-03-10"""
-                + Markdown.LINEBREAK_TYPE + Markdown.METADATA_SEPARATOR + Markdown.LINEBREAK_TYPE +
-                """
-                # Mon premier article
-                ## Mon sous-titre
-                Le contenu de mon article.
-                ![Une image](./image.png)""";
+        String markDownWithMetadata = "titre: Mon premier article\n"
+                + "auteur: Bertil Chapuis\n"
+                + "date: 2021-03-10\n"
+                + Markdown.LINEBREAK_TYPE + Markdown.METADATA_SEPARATOR + Markdown.LINEBREAK_TYPE
+                + "# Mon premier article\n"
+                + "## Mon sous-titre\n"
+                + "Le contenu de mon article.\n"
+                + "![Une image](./image.png)";
 
         String[] expectedOutput = {
-                """
-                titre: Mon premier article
-                auteur: Bertil Chapuis
-                date: 2021-03-10""",
-                """ 
-                # Mon premier article
-                ## Mon sous-titre
-                Le contenu de mon article.
-                ![Une image](./image.png)"""
+                "titre: Mon premier article\n"
+                        + "auteur: Bertil Chapuis\n"
+                        + "date: 2021-03-10\n",
+                "# Mon premier article\n"
+                        + "## Mon sous-titre\n"
+                        + "Le contenu de mon article.\n"
+                        + "![Une image](./image.png)"
         };
 
         String[] actualOutput = markdown.getMetadata(markDownWithMetadata);
@@ -134,39 +118,34 @@ public class MarkdownTest {
 
     @Test
     public void getMetadataShouldReturnRuntimeErrorWhenNoOrWrongSeparatorInGivenMarkdown() {
-        String markDownWithMetadataButNoSeparator =
-                """
-                titre: Mon premier article
-                auteur: Bertil Chapuis
-                date: 2021-03-10
-                # Mon premier article
-                ## Mon sous-titre
-                Le contenu de mon article.
-                ![Une image](./image.png)""";
+        String markDownWithMetadataButNoSeparator = "titre: Mon premier article\n"
+                + "auteur: Bertil Chapuis\n"
+                + "date: 2021-03-10\n"
+                + "# Mon premier article\n"
+                + "## Mon sous-titre\n"
+                + "Le contenu de mon article.\n"
+                + "![Une image](./image.png)";
 
-        String markDownWithMetadataButWrongSeparator =
-                """
-                titre: Mon premier article
-                auteur: Bertil Chapuis
-                date: 2021-03-10
-                --oe-
-                # Mon premier article
-                ## Mon sous-titre
-                Le contenu de mon article.
-                ![Une image](./image.png)""";
+        String markDownWithMetadataButWrongSeparator = "titre: Mon premier article\n"
+                + "auteur: Bertil Chapuis\n"
+                + "date: 2021-03-10\n"
+                + "--oe-\n"
+                + "# Mon premier article\n"
+                + "## Mon sous-titre\n"
+                + "Le contenu de mon article.\n"
+                + "![Une image](./image.png)";
 
-        String markDownWithMetadataButMisplacedSeparator =
-                """
-                titre: Mon premier article
-                auteur: Bertil Chapuis
-                date: 2021-03-10-oe--
-                # Mon premier article
-                ## Mon sous-titre
-                Le contenu de mon article.
-                ![Une image](./image.png)""";
+        String markDownWithMetadataButMisplacedSeparator = "titre: Mon premier article\n"
+                + "auteur: Bertil Chapuis\n"
+                + "date: 2021-03-10-oe--\n"
+                + "# Mon premier article\n"
+                + "## Mon sous-titre\n"
+                + "Le contenu de mon article.\n"
+                + "![Une image](./image.png)";
         
         assertThrows(RuntimeException.class,() -> markdown.getMetadata(markDownWithMetadataButNoSeparator));
         assertThrows(RuntimeException.class,() -> markdown.getMetadata(markDownWithMetadataButWrongSeparator));
         assertThrows(RuntimeException.class,() -> markdown.getMetadata(markDownWithMetadataButMisplacedSeparator));
     }
+
 }
