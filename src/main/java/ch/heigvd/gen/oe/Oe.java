d59cf0e1be015fdafb2eb83f48c2bb2c4acdc335
K-do <miguel.dovalelopes@heig-vd.ch>
2021-04-03 15:11:15 +0200

    added resources dir, changed pom.xml to filter resources and add option '--version/-v'

--- src/main/java/ch/heigvd/gen/oe/Oe.java
+++ src/main/java/ch/heigvd/gen/oe/Oe.java
@@ -4,15 +4,20 @@ import ch.heigvd.gen.oe.command.Build;
 import ch.heigvd.gen.oe.command.Clean;
 import ch.heigvd.gen.oe.command.Init;
 import ch.heigvd.gen.oe.command.Serve;
+
+import java.util.Properties;
 import java.util.concurrent.Callable;
+
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
-@Command(
-        name = "projet-oe",
+@Command(name = "projet-oe",
         description = "A brand new static site generator.",
         subcommands = {Init.class, Clean.class, Build.class, Serve.class})
 public class Oe implements Callable<Integer> {
+
+    @CommandLine.Option(names = {"-v", "--version"}, description = "version of oe static site generator")
+    static boolean version;
 
     public static void main(String... args) {
         int exitCode = new CommandLine(new Oe()).execute(args);
@@ -21,7 +26,18 @@ public class Oe implements Callable<Integer> {
 
     @Override
     public Integer call() throws Exception {
-        CommandLine.usage(this, System.out);
+
+        // Display version if requested
+        if (version) {
+            final Properties properties = new Properties();
+            properties.load(this.getClass().getClassLoader().getResourceAsStream("oe.properties"));
+            System.out.println("oe - " + properties.getProperty("version"));
+        }
+
+        // No Valid params, display usage
+        else {
+            CommandLine.usage(this, System.out);
+        }
         return 0;
     }
 }
