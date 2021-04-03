41302ff9ca59f69a55202a4c81c70c087c897633
FionaGbn <fiona.gamboni@heig-vd.ch>
2021-04-03 13:31:45 +0200

    Added commentaries in DFSFileExplorer

--- src/main/java/ch/heigvd/gen/oe/utils/DFSFileExplorer.java
+++ src/main/java/ch/heigvd/gen/oe/utils/DFSFileExplorer.java
@@ -2,16 +2,26 @@ package ch.heigvd.gen.oe.utils;
 
 import java.io.File;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.Arrays;
-import java.util.concurrent.Callable;
 import java.util.function.Consumer;
 
+
+/**
+ * Class DFSFileExplorer to DFS a file system in pre or post order
+ * 
+ * authors: Gamboni Fiona, Do Vale Lopes Miguel
+ */
 public class DFSFileExplorer {
 
     private final Consumer<File> function;
     private final boolean pre;
 
+    /**
+     * Constructor
+     *
+     * @param function function to use while exploring
+     * @param pre      boolean to tell if function will be used in pre order (false for post order)
+     */
     public DFSFileExplorer(Consumer<File> function, boolean pre) {
         if (function == null) {
             throw new NullPointerException("Function must exist");
@@ -20,6 +30,13 @@ public class DFSFileExplorer {
         this.pre = pre;
     }
 
+    /**
+     * Visit file system and apply function to each file
+     *
+     * @param root root directory
+     * @throws InvocationTargetException - if function could not be invoked
+     * @throws IllegalAccessException    - if function could not be accessed
+     */
     public void visit(File root) throws InvocationTargetException, IllegalAccessException {
         if (root == null) {
             throw new NullPointerException("Root must exist");
