8f6f49fadcd7691f4da82beb9d0125b837c66aa2
Gaétan Zwick <gaetan.zwick@heig-vd.ch>
2021-04-30 11:30:20 +0200

    added base of serve function

--- src/main/java/ch/heigvd/gen/oe/command/Serve.java
+++ src/main/java/ch/heigvd/gen/oe/command/Serve.java
@@ -1,14 +1,22 @@
 package ch.heigvd.gen.oe.command;
 
 import java.util.concurrent.Callable;
+
+import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
 @Command(name = "serve", description = "Serve a static site")
 public class Serve implements Callable<Integer> {
+    @CommandLine.Parameters(paramLabel = "</path/site>", description = "directory to build site")
+    private String dirSiteName;
 
     @Override
     public Integer call() throws Exception {
-        System.out.printf("build");
+
+        SimpleHttpServer server = new SimpleHttpServer(dirSiteName);
+
+        server.start();
+
         return 1;
     }
 }
