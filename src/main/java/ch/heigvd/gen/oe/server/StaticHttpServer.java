78a7805c40a1961d862d963291ec9fc8abcb276e
Gaétan Zwick <gaetan.zwick@heig-vd.ch>
2021-05-14 12:17:02 +0200

    added comments, removed while loop in serve.java

--- src/main/java/ch/heigvd/gen/oe/server/StaticHttpServer.java
+++ src/main/java/ch/heigvd/gen/oe/server/StaticHttpServer.java
@@ -9,16 +9,24 @@ import java.net.InetSocketAddress;
 
 public class StaticHttpServer {
 
+    // base directory where the build folder should exist
     private static String BASEDIR;
 
+    // default port 8080
     private static final int PORT = 8080;
 
+    // instance of the http server
     private HttpServer server;
 
     public StaticHttpServer(String baseDir) {
         BASEDIR = baseDir;
     }
 
+    /**
+     * Called to start a new server
+     *
+     * @return true if the server started without problems otherwise throw an IOException
+     */
     public boolean start() throws IOException {
         // new http server
         server = HttpServer.create(new InetSocketAddress(PORT), 0);
@@ -26,6 +34,8 @@ public class StaticHttpServer {
         // check if BASEDIR/build exist
         File file = new File(BASEDIR + "/build");
         file.createNewFile();
+
+        // if no build directory exists at BASEDIR
         if (!file.exists())
             throw new FileNotFoundException("The specified directory does not contains a \"build\" folder\nServer not started");
 
@@ -37,6 +47,9 @@ public class StaticHttpServer {
         return true;
     }
 
+    /**
+     * Called to stop the server
+     */
     public void stop() {
         server.stop(0);
     }
