39c7d35d73f163427a118e732b8fac58a9f5fba9
FionaGbn <fiona.gamboni@heig-vd.ch>
2021-05-29 12:56:37 +0200

    Finished benchmarks

--- src/main/java/ch/heigvd/gen/oe/benchmark/BenchmarkRunner.java
+++ src/main/java/ch/heigvd/gen/oe/benchmark/BenchmarkRunner.java
@@ -5,6 +5,11 @@ import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
+/**
+ * class to run Benchmarks on project Oe
+ *
+ * authors: Do Vale Lopes Miguel, Gamboni Fiona
+ */
 public class BenchmarkRunner {
     public static void main(String[] args) throws RunnerException {
         Options options = new OptionsBuilder()
@@ -13,6 +18,5 @@ public class BenchmarkRunner {
                 .measurementIterations(1)
                 .build();
         new Runner(options).run();
-
     }
 }
--- src/main/java/ch/heigvd/gen/oe/benchmark/OeBenchmark.java
+++ src/main/java/ch/heigvd/gen/oe/benchmark/OeBenchmark.java
@@ -1,12 +1,20 @@
 package ch.heigvd.gen.oe.benchmark;
 
 import ch.heigvd.gen.oe.Oe;
+import ch.heigvd.gen.oe.utils.DFSFileExplorer;
 import org.openjdk.jmh.annotations.*;
 import picocli.CommandLine;
 
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
 import java.util.concurrent.TimeUnit;
 
-@BenchmarkMode(Mode.SingleShotTime)
+/**
+ * Class regrouping benchmarks on project oe
+ *
+ * authors: Do Vale Lopes Miguel, Gamboni Fiona
+ */
+@BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @State(Scope.Benchmark)
 public class OeBenchmark {
@@ -19,10 +27,17 @@ public class OeBenchmark {
     }
 
     @Benchmark
-    public void benchmarkBuild() {
+    public void build() {
         new CommandLine(new Oe()).execute("build", DIRNAME);
     }
 
-
-
+    @TearDown
+    public void clean() {
+        DFSFileExplorer dfsPost = new DFSFileExplorer(File::delete, false);
+        try {
+            dfsPost.visit(new File(DIRNAME));
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
 }
